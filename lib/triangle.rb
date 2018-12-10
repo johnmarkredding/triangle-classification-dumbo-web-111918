9303fcf27ab96cac1eb09f9a9127d9e351c4f604
John Mark Redding <johnmarkredding@gmail.com>
2018-12-10 16:22:43 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+  @kind
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+  
+  def kind
+    if self.side1 + self.side2 > self.side3
+      :equilateral
+      :isosceles
+      :scalene
+    else
+      raise TriangleError
+    end
+    
+    begin
+        raise TriangleError
+    rescue TriangleError => error
+      puts error.message
+    end
+  end
+  
+  class TriangleError < StandardError
+    def message
+      ""
+    end
+  end
 end
