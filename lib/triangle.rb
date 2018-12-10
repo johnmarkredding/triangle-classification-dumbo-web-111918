4340ab8fdb66da5478b92dce3af035fd45aa7058
John Mark Redding <johnmarkredding@gmail.com>
2018-12-10 18:16:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,45 @@
 class Triangle
-  # write code here
+  attr_accessor :sides
+  
+  def initialize(*args)
+    @sides = [args]
+  end
+  
+  def equilateral?
+    self.sides[0] == self.sides[1] && self.sides[1] == self.sides[2]
+  end
+  def isosceles?
+    self.sides[0] == self.sides[1] || self.sides[1] == self.sides[2] || self.sides[0] == self.sides[2]
+  end
+  def scalene?
+    self.sides[0] != self.sides[1] && self.sides[1] != self.sides[2]
+  end
+  def triangle?
+    (self.sides[0] > 0 && self.sides[1] > 0 self.sides[0]) && (self.sides[0] + self.sides[1] > self.sides[2]) && ()
+  end
+  
+  def kind
+    if self.triangle?
+      if self.equilateral?
+        @kind = :equilateral
+      elsif self.isosceles?
+        @kind = :isosceles
+      elsif self.scalene?
+        @kind = :scalene
+      end
+      @kind
+    else
+      begin
+          raise TriangleError
+      rescue TriangleError => error
+        puts error.message
+      end
+    end
+  end
+  
+  class TriangleError < StandardError
+    def message
+      "Not a triangle."
+    end
+  end
 end
